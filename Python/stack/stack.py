d6d205825527249570588d19fdfd1e68af64388e
drewkiimon <andrew.r.pagan@gmail.com>
2017-03-08 14:12:38 -0800

    Testing

--- Python/stack/stack.py
+++ Python/stack/stack.py
@@ -1,3 +1,25 @@
 # Stacks are thought of like a stack of paper
 # First in is last out
 # LIFO -> Last In, First Out
+
+class Stack:
+	def __init__(self):
+		self.items = []
+	
+	def push(self, item):
+		self.items.append(item)
+	
+	def pop(self):
+		ret = self.items[-1]
+		self.items = self.items[:-1]
+		return ret
+	
+	def size(self):
+		return len(self.items)
+	
+	def dump(self):
+		self.items = []
+	
+	def view(self):
+		print self.items
+	
