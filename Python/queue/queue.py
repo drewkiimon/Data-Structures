90e1083115a218756edaed856b9f987ccb0edeab
drewkiimon <andrew.r.pagan@gmail.com>
2017-03-08 15:32:07 -0800

    New new

--- Python/queue/queue.py
+++ Python/queue/queue.py
@@ -1,3 +1,31 @@
 # Queues are more like a line at a grocery store
 # It's fair as in first one in, first one out
 # Aka FIFO
+
+class Queue:
+	def __init__(self):
+		self.items = []
+		
+	def empty(self):
+		return len(self.items) == 0
+		
+	def size(self):
+		return len(self.items)
+
+	def front(self):
+		return self.items[0]
+
+	def back(self):
+		return self.items[-1]
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		ret = self.items[0]
+		self.items = self.items[1:]
+		return ret
+
+	def view(self):
+		print self.items
+		
--- Python/queue/test.py
+++ Python/queue/test.py
@@ -1,1 +1,18 @@
 # Test for queue
+
+from queue import *
+
+x = Queue()
+print x.empty()
+x.push(3)
+x.push(10)
+x.push(4)
+x.push(7)
+x.push(-2)
+x.view()
+print x.size()
+print x.front()
+print x.back()
+print x.pop()
+print x.pop()
+x.view()
